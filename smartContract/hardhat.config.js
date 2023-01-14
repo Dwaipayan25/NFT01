1ac8d488e25989d299f0f260652a7407af9e48a7
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2023-01-14 12:25:16 +0530

    Contract Deployed and NFT minted successfully

--- smartContract/hardhat.config.js
+++ smartContract/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "mumbai",
+  networks: {
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/eyoxJkmZpXOfJmP_SfhY8zBmGrTmAWIj",
+      accounts: ["a7302f7b6ebaf0a78643ec610f30185fa058ba45ef137ba99fdcd4b07af858c1"],
+    }
+  }
 };
